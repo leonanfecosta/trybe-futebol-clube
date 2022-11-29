abedbc28ee15d01c87c44faed7b888ae3156aee9
Leonan Costa <leonanfecosta@gmail.com>
2022-11-29 12:30:37 -0300

    Update database.ts

--- app/backend/src/database/config/database.ts
+++ app/backend/src/database/config/database.ts
@@ -2,11 +2,12 @@ import 'dotenv/config';
 import { Options } from 'sequelize';
 
 const config: Options = {
-  username: process.env.DB_USER || 'root',
-  password: process.env.DB_PASS || '123456',
-  database: 'TRYBE_FUTEBOL_CLUBE',
-  host: process.env.DB_HOST || 'localhost',
-  port: Number(process.env.DB_PORT) || 3002,
+  username: process.env.MYSQLUSER || 'root',
+  password: process.MYSQLPASSWORD || '123456',
+  database: process.env.MYSQLDATABASE || 'TRYBE_FUTEBOL_CLUBE',
+  host: process.env.MYSQLHOST || 'localhost',
+  port: Number(process.env.MYSQLPORT) || 3002,
+  url: process.env.MYSQL_URL,
   dialect: 'mysql',
   dialectOptions: {
     timezone: 'Z',
