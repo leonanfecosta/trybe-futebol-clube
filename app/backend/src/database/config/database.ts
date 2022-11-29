7eb10e71bb6cf65a94cf0bc30771e925cd375ac8
Leonan Costa <leonanfecosta@gmail.com>
2022-11-29 12:51:12 -0300

    Update database.ts

--- app/backend/src/database/config/database.ts
+++ app/backend/src/database/config/database.ts
@@ -3,7 +3,7 @@ import { Options } from 'sequelize';
 
 const config: Options = {
   username: process.env.MYSQLUSER || 'root',
-  password: process.MYSQLPASSWORD || '123456',
+  password: process.env.MYSQLPASSWORD || '123456',
   database: process.env.MYSQLDATABASE || 'TRYBE_FUTEBOL_CLUBE',
   host: process.env.MYSQLHOST || 'localhost',
   port: Number(process.env.MYSQLPORT) || 3002,
